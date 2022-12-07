855cae86573c03349b38538dd9ce5772a2336361
Dennis Njogu <dennis.njogu@student.moringaschool.com>
2022-12-08 02:28:41 +0300

    followed along

--- src/reducer.js
+++ src/reducer.js
@@ -1,9 +1,11 @@
 let state = { count: 0 };
 
-function changeState(state, action) {
+//doing this to our state argument helps us establish a default state when its undefined
+function changeState(state = { count: 0 }, action) {
   switch (action.type) {
     case "counter/increment":
       return { count: state.count + 1 };
+    //this will return if we havent defined an action in our reducer
     default:
       return state;
   }
